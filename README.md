72596eb61b7a5495b618ddc46f839eabbd187b1f
Linavo <linaqvo@gmail.com>
2024-08-29 15:55:30 -0700

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # image-carousel
+
+# Photo Credits
+
+Photo by <a href="https://unsplash.com/@lifeof_peter_?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Peter Thomas</a> on <a href="https://unsplash.com/photos/a-group-of-palm-trees-with-a-sunset-in-the-background-Zn0WtisiNqY?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a>
+
+Photo by <a href="https://unsplash.com/@zeedoesnotcare?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Mir Ali</a> on <a href="https://unsplash.com/photos/a-body-of-water-with-trees-and-grass-around-it-xxBBnEtk0IY?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a>
+
+Photo by <a href="https://unsplash.com/@tannerrmorann__?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">tanner moran</a> on <a href="https://unsplash.com/photos/green-mountain-under-blue-sky-during-daytime-3im2eVihGe0?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a>
---
+++ main.js
@@ -0,0 +1,29 @@
+const buttons = document.querySelectorAll("[data-carousel-button]");
+
+// I'm using datasets instead of classes to grab nodes to not confuse when grabbing classes
+buttons.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    const offset = button.dataset.carouselButton === "next" ? 1 : -1;
+
+    // closest() searches up the DOM tree which matches closest selector
+    // Dong this will ensure that no matter how many carousels wer have on our page, all of them will work fine (for re-use for larger web pages)
+    const slides = button
+      .closest("[data-carousel]")
+      .querySelector("[data-slides]");
+
+    const activeSlide = slides.querySelector("[data-active]");
+
+    // Use the spread operator to grab the array of children of the UL, and then grab the index of the current slide with the data-active currently. Add the offset depending on whether the user hits next or previous arrow.
+    let newIndex = [...slides.children].indexOf(activeSlide) + offset;
+
+    if (newIndex < 0) {
+      newIndex = slides.children.length - 1;
+    }
+    if (newIndex >= slides.children.length) {
+      newIndex = 0;
+    }
+
+    slides.children[newIndex].dataset.active = true;
+    delete activeSlide.dataset.active;
+  });
+});
